ccee48784498a31706f2a42c18ac3112755b09e2
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2024-06-24 21:30:11 +0900

    mainの実装

---
+++ invoice/main.rb
@@ -0,0 +1,13 @@
+require_relative './sales_report_generator'
+
+sales_data = [
+  { date: '2023-03-01', amount: 1000 },
+  { date: '2023-03-02', amount: 1500 },
+  { date: '2023-03-03', amount: 1200 }
+]
+
+generator = SalesReportGenerator.new(sales_data)
+
+puts generator.generate_report('TEXT')
+puts generator.generate_report('HTML')
+puts generator.generate_report('JSON')
