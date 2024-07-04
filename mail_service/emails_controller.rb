55a206ecb078d8527f14d88641765275f0a0738e
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2024-07-05 05:21:22 +0900

    categorizedのメソッドを追加

--- mail_service/emails_controller.rb
+++ mail_service/emails_controller.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 class Api::V1::MailService::EmailsController < Api::V1::BaseController
   def categorize
     user = User.find(params[:user_id])
-    emails = user.emails.where.not(category: %i[archieved draft])
 
-    
+    categorized_emails = user.emails.where.not(category: %i[archieved deleted]).catedorized_emails
+
+    render json: Email.format(categorized_emails, :json)
   end
 end
