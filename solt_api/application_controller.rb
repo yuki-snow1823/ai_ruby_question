b24babc2bf0ad11c7bab74e0406db11b626bbeec
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2024-07-21 14:53:25 +0900

    JWTの基本認証を追加

---
+++ solt_api/application_controller.rb
@@ -0,0 +1,28 @@
+class ApplicationController < ActionController::API
+  SECRET_KEY = Rails.application.credentials.secret_key_base
+
+  private
+
+  def authenticate_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      @decoded = jwt_decode(header)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
+
+  def jwt_encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def jwt_decode(token)
+    decoded = JWT.decode(token, SECRET_KEY)[0]
+    HashWithIndifferentAccess.new decoded
+  end
+end
