35f7eacbde1040169d0c3fbe19dc384c12039cbc
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2024-07-25 21:49:17 +0900

    整数問題

---
+++ number_control/index.rb
@@ -0,0 +1,10 @@
+numbers = gets.split.map(&:to_i)
+
+
+def process_integers(numbers)
+  doubled_numbers = numbers.map{ |n| n * 2 }
+  plus_minus_numbers = doubled_numbers.map{ |n| n.even? ? n + 1 : n - 2 }
+  plus_minus_numbers.sum
+end
+
+puts process_integers(numbers)
