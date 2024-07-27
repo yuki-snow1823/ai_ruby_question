16a222bedc81256caf38bf2e174a6d9b604740b9
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2024-07-28 08:34:16 +0900

    rubocop

--- number_control/index.rb
+++ number_control/index.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 numbers = gets.split.map(&:to_i)
 
-
 def process_integers(numbers)
-  doubled_numbers = numbers.map{ |n| n * 2 }
-  plus_minus_numbers = doubled_numbers.map{ |n| n.even? ? n + 1 : n - 2 }
+  doubled_numbers = numbers.map { |n| n * 2 }
+  plus_minus_numbers = doubled_numbers.map { |n| n.even? ? n + 1 : n - 2 }
   plus_minus_numbers.sum
 end
 
